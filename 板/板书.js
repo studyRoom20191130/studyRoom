46d0726e3641622994981de13c0cc4ff755ec881
theLongGoodbye <2518921392@qq.com>
2020-03-24 13:10:16 +0800

    上传临时文件

---
+++ 板/板书.js
@@ -0,0 +1,109 @@
+图形渲染  axe15
+图像压缩
+音频处理
+视频压缩
+
+预习《opengl 编程指南》一小时
+
+
+现代图形学，分两块
+1，实时渲染
+    特点是快速
+    可以牺牲质量换速度
+2，离线渲染
+    影视特效
+    动画
+    特点是高质量，可以牺牲速度换质量
+
+
+学习难度
+    很难
+
+3D 打砖块
+https://www.zhihu.com/pin/1066776886746775552
+
+3d guagame，运行时热重载添加地板换纹理
+https://www.zhihu.com/pin/1144753099506970624
+
+3d guagame，动态游戏逻辑
+准大学生，如何预习计算机专业？ - 萧井陌的回答 - 知乎
+https://www.zhihu.com/question/24188466/answer/787366266
+
+萧井陌的想法：斧头班同学上了一节图形课后自制的光照，虽然不对但是效果还不错
+https://www.zhihu.com/pin/1065620711548243968
+
+
+图形学基础——画点
+光栅化
+SONY PS3
+
+光栅化渲染器
+用软件实现显卡的功能
+硬件加速
+opengl dx
+
+
+一个箱子有 8 个顶点
+
+
+matrix 矩阵
+
+
+
+图形学是很 tricky 的领域
+有问题，直接 chat 见
+我亲自回答每个问题
+一个点为什么会有法向量
+
+
+管线 pipeline
+固定管线 fixed pipeline
+可编程管线 shader   shading language
+vertex shader
+pixel shader(fragment shader)
+
+为什么要叫 shader（不要问为什么，它就是 shader）
+
+shader 的学习
+十分钟
+《opengl shading language》橙皮书
+
+
+
+主流的技术
+opengl
+directx
+手机 opengl es（嵌入式系统）
+浏览器 webgl（opengles）
+opengl4
+dx12
+新的 vulkan
+苹果 metal
+
+
+OpenGL 是基于状态机的模型
+状态机 就是全局变量
+
+
+drawRect(1, 2, 3, 4， red)
+
+
+OpenGL matrix
+
+m = LookAtLH()
+translate(m)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+。
